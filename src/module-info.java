61dc5847a64109222eef938b0157f8dfe63cda5a
EmreHamatoglu <hamatogluemre@hotmail.com>
2022-10-16 22:19:23 +0300

    Create Project

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hamat
+ *
+ */
+module test2 {
+}
---
+++ src/test2_Intro/Main2.java
@@ -0,0 +1,13 @@
+package test2_Intro;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
